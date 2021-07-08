864479bd11e390fe370834b0d0888766e2940322
jokokendi <80366713+jokokendi@users.noreply.github.com>
2021-07-08 10:48:48 +0700

    Update private.py

--- MusicKen/modules/private.py
+++ MusicKen/modules/private.py
@@ -51,7 +51,7 @@ disable_web_page_preview=True,
         reply_to_message_id=message.message_id
         )
 
-@Client.on_message(filters.private & filters.incoming & filters.command & filters.group & ~ filters.edited(['help']))
+@Client.on_message(filters.private & filters.incoming & filters.command & filters.group (['help']))
 def _help(client, message):
     client.send_message(chat_id = message.chat.id,
         text = tr.HELP_MSG[1],
@@ -97,52 +97,6 @@ def map(pos):
             ],
         ]
     return button
-@Client.on_message(filters.group & ~ filters.edited & filters.command("help"))
-def _help(client, message):
-    client.send_message(chat_id = message.chat.id,
-        text = tr.HELP_MSG[1],
-        parse_mode="markdown",
-        disable_web_page_preview=True,
-        disable_notification=True,
-        reply_markup = InlineKeyboardMarkup(map(1)),
-        reply_to_message_id = message.message_id
-    )
-
-help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
-
-@Client.on_callback_query(help_callback_filter)
-def help_answer(client, callback_query):
-    chat_id = callback_query.from_user.id
-    disable_web_page_preview=True
-    message_id = callback_query.message.message_id
-    msg = int(callback_query.data.split('+')[1])
-    client.edit_message_text(chat_id=chat_id,    message_id=message_id,
-        text=tr.HELP_MSG[msg],    reply_markup=InlineKeyboardMarkup(map(msg))
-    )
-
-def map(pos):
-    if(pos==1):
-        button = [
-            [InlineKeyboardButton(text = '▶️', callback_data = "help+2")]
-        ]
-    elif(pos==len(tr.HELP_MSG)-1):
-        url = f"https://t.me/{SUPPORT_GROUP}"
-        button = [
-            [InlineKeyboardButton("➕ Tambahkan saya ke Grup Anda ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-            [InlineKeyboardButton(text = '📣 CHANNEL', url=f"https://t.me/{UPDATES_CHANNEL}"),
-             InlineKeyboardButton(text = '💬 GROUP', url=f"https://t.me/{SUPPORT_GROUP}")],
-            [InlineKeyboardButton(text = '🛠 CLONE 🛠', url=f"https://{SOURCE_CODE}")],
-            [InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}")]
-        ]
-    else:
-        button = [
-            [
-                InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}"),
-                InlineKeyboardButton(text = '▶️', callback_data = f"help+{pos+1}")
-            ],
-        ]
-    return button
-
 
 @Client.on_message(filters.command("start") & filters.group & ~ filters.edited)
 async def start(client: Client, message: Message):
