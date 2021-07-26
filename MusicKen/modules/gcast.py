820ce27e830dc8cea18167465b6411d56c3268ac
jokokendi <80366713+jokokendi@users.noreply.github.com>
2021-07-26 21:31:21 +0700

    Update gcast.py

--- MusicKen/modules/gcast.py
+++ MusicKen/modules/gcast.py
@@ -4,7 +4,7 @@ from pyrogram.types import Dialog, Chat, Message
 from pyrogram.errors import UserAlreadyParticipant
 from MusicKen.config import SUDO_USERS
 from MusicKen.helpers.filters import command
-from MusicKen.config import BOT_TOKEN as USER
+from MusicKen.services.callsmusic.callsmusic import client as USER
 
 @Client.on_message(command("gcast") & filters.user(SUDO_USERS) & ~filters.edited)
 async def gcast(_, message: Message):
